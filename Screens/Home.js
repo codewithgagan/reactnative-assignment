9b974d470433b0bd0843ee6d80693de43cb45c7f
Malavika S Menon <malavikamenon2000@gmail.com>
2020-09-14 12:49:08 +0530

    Data fetch done

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,19 +1,55 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView, ScrollView, Text, Platform, StatusBar} from "react-native";
+import {StyleSheet, SafeAreaView, ScrollView, Text, Platform, StatusBar, FlatList, View, ActivityIndicator} from "react-native";
 import Card from "../components/Card";
+import {getArticles} from '../fetchNews';
+//import {Container, Content, List} from 'native-base';
 
-function Home(props) {
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoading: true,
+      data: null
+    }
+  }
+
+  componentDidMount(){
+    getArticles().then(data => {
+      this.setState({
+        isLoading: false,
+        data: data
+      });
+    }, error => {
+      alert("Error Something went wrong");
+    }
+    )
+  }
+  render(){
+    alert(this.state.data);
+    let view = this.state.isLoading ? (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator animating={this.state.isLoading} color="#00f0ff" />
+        <Text style={{marginTop: 10}} children="Please Wait.." />
+      </View>
+    ) : (
+      <FlatList
+        dataArray={this.state.data}
+        renderItem={(item) => {
+            return (
+              <Card  data={item} />
+            )
+        }} />
+    )
+    console.log(view.props);
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView contentContainerStyle={styles.centerAlign}>
                 <Text style={styles.heading}>Welcome To our App</Text>
-                <Card props={props} />
-                <Card props={props} />
-                <Card props={props} />
-                <Card props={props} />
+               {view}
             </ScrollView>
         </SafeAreaView>
-    )
+    );
+    }
 }
 
 const styles = StyleSheet.create({
--- components/Card.js
+++ components/Card.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import {Image, View, Button, Text, StyleSheet} from "react-native";
 
-function Card(props) {
-    
-    return (
-        <View style={styles.card}>
-            <Image style={styles.image} source={{
-                height: 300,
-                width: 300,
-                uri: "http://placeimg.com/640/480/food"
-            }} />
-            <Text style={styles.title}>Tanya Rempel</Text>
-            <Text style={styles.content}>Lorem ipsum dolor sit amet, adipiscing</Text>
-            <Button onPress={() => {props.props.navigation.navigate("Detail")}} title="Read More ..." />
-        </View>
-    )
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.data = props.data;
+    }
+
+    render(){
+        return (
+            <View style={styles.card}>
+                <Image style={styles.image} source={{
+                    height: 300,
+                    width: 300,
+                    uri: /*this.data.urlToImage != null ? this.data.urlToImage: */ "http://placeimg.com/640/480/food"
+                }} />
+                alert
+                <Text style={styles.title}>{this.data.title}</Text>
+            <Text style={styles.content}>{this.data.description}</Text>
+                <Button onPress={() => {props.props.navigation.navigate("Detail")}} title="Read More ..." />
+            </View>
+        )
+    }
 }
 const styles = StyleSheet.create({
     title: {
